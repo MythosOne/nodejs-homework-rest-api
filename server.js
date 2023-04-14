8c0e3e66014b172a19a6b0964235e283dee9bbcf
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-04-14 12:08:10 +0300

    nodejs-homework-rest-api-env

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Mythos:PkwdS0wuD8xsWK8x@cluster0.ozn3mcl.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
+
+
 
 mongoose
   .connect(DB_HOST)
@@ -19,5 +20,10 @@ mongoose
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
 // });
+
+// .then(
+//      () => {app.listen(3000)
+//       console.log("Database connection successful");
+//     })
 
 // PkwdS0wuD8xsWK8x
